307f7adbed4e8900106f15e1b2fded3884319f7c
Chiril Gorbenco <chiril.gorbenco@student.upt.ro>
2023-11-30 01:16:15 +0200

    home page

--- sections/header/header.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta, Story } from '@storybook/react';
-import { Header as Component } from './header.section';
-
-export default {
-  title: 'Header',
-  component: Component,
-} as Meta;
-
-export const Header: Story = () => <Component />;
